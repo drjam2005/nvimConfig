5da22caeee20bc9a3381e598d12bd608d0ce46ea
drjam2005 <jamesallanmdr2005@gmail.com>
2025-05-16 22:04:10 +0800

    moving shit

--- lua/vim-config.lua
+++ lua/vim-config.lua
@@ -8,6 +8,14 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
 
+vim.keymap.set('i', '<C-h>', '<C-Left')
+vim.keymap.set('i', '<C-j>', '<Down>')
+vim.keymap.set('i', '<C-k>', '<Up>')
+vim.keymap.set('i', '<C-l>', '<C-Right')
+
 vim.opt.wrap = false
 vim.keymap.set('n', 'L', 'zl')
 vim.keymap.set('n', 'H', 'zh')
+
+
+vim.cmd("set scrolloff=5")
