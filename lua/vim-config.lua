63fb8ed13e1efc17d4e532a40d6ba9f0db4d5f57
drjam2005 <jamesallanmdr2005@gmail.com>
2025-05-16 22:06:36 +0800

    update

--- lua/vim-config.lua
+++ lua/vim-config.lua
@@ -8,7 +8,7 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
 
-vim.keymap.set('i', '<C-h>', '<C-Left')
+vim.keymap.set('i', '<C-h>', '<C-Left>')
 vim.keymap.set('i', '<C-j>', '<Down>')
 vim.keymap.set('i', '<C-k>', '<Up>')
 vim.keymap.set('i', '<C-l>', '<C-Right>')
