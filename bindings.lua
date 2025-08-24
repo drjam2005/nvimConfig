2d7df7eb716c67c949e9815acba7db72c7283cc7
drjam2005 <jamesallanmdr2005@gmail.com>
2025-08-24 14:25:56 +0800

    okayy?

--- bindings.lua
+++ bindings.lua
@@ -1,4 +1,5 @@
 vim.cmd("set shiftwidth=4")
+vim.cmd("set tabstop=4")
 vim.cmd("set relativenumber")
 vim.cmd("set number")
 vim.cmd("command W w")
@@ -9,10 +10,10 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', opts)
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', opts)
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', opts)
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', opts)
-vim.keymap.set('n', '<C-M-h>', '<C-w><S-h>', opts)
-vim.keymap.set('n', '<C-M-j>', '<C-w><S-j>', opts)
-vim.keymap.set('n', '<C-M-k>', '<C-w><S-k>', opts)
-vim.keymap.set('n', '<C-M-l>', '<C-w><S-l>', opts)
+vim.keymap.set('n', '<c-m-h>', '<c-w><s-h>', opts)
+vim.keymap.set('n', '<c-m-j>', '<c-w><s-j>', opts)
+vim.keymap.set('n', '<c-m-k>', '<c-w><s-k>', opts)
+vim.keymap.set('n', '<c-m-l>', '<c-w><s-l>', opts)
 vim.keymap.set('n', '<Esc>', '<Esc>:noh<cr>', opts)
 
 vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
@@ -22,6 +23,11 @@ vim.keymap.set('i', '<C-h>', '<C-Left>', opts)
 vim.keymap.set('i', '<C-j>', '<Down>', opts)
 vim.keymap.set('i', '<C-k>', '<Up>', opts)
 vim.keymap.set('i', '<C-l>', '<C-Right>', opts)
+
+vim.keymap.set('t', '<C-h>', '<C-\\><C-n><C-w><C-h><C-w><C-h>', opts)
+vim.keymap.set('t', '<C-j>', '<C-\\><C-n><C-w><C-h><C-w><C-j>', opts)
+vim.keymap.set('t', '<C-k>', '<C-\\><C-n><C-w><C-h><C-w><C-k>', opts)
+vim.keymap.set('t', '<C-l>', '<C-\\><C-n><C-w><C-h><C-w><C-l>', opts)
 
 vim.o.wrap = false
 vim.o.signcolumn = "yes"
